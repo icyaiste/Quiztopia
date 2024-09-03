8247260936cc969a3b80edca4285d5ed25e809fb
Aiste <icy.aiste@gmail.com>
2024-09-03 11:14:02 +0200

    position fetching before map initialization

--- Quiztopia/src/components/map/Leaflet.tsx
+++ Quiztopia/src/components/map/Leaflet.tsx
@@ -32,37 +32,34 @@ function Leaflet({ setPosition }: LeafletProps) {
     }, []);
 
     useEffect(() => {
-        if (myLocation) {
-            console.log('Location i s set', myLocation);
+        if (myLocation && !mapRef.current) {
             const { latitude, longitude } = myLocation;
-            if (latitude && longitude && mapRef.current === null)
-                console.log('Creating map with location:', latitude, longitude);
-            { // Only initialize the map if it hasn't been initialized yet
-                const myMap = leaflet.map('map').setView([myLocation.latitude, myLocation.longitude], 15);
-                leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 19,
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                })
-                    .addTo(myMap);
-                const marker = leaflet.marker([myLocation.latitude, myLocation.longitude]).addTo(myMap);
-                marker.bindPopup('You are here!').openPopup();
+            console.log('Creating map with location:', latitude, longitude);
 
+            // Initialize the map
+            const myMap = leaflet.map('map').setView([latitude, longitude], 15);
 
-                // Force map to recalculate its size
-                myMap.invalidateSize();
+            leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+            }).addTo(myMap);
 
+            const marker = leaflet.marker([latitude, longitude]).addTo(myMap);
+            marker.bindPopup('You are here!').openPopup();
 
-                mapRef.current = myMap;
+            // Force map to recalculate its size
+            myMap.invalidateSize();
 
-                setMap(myMap);
+            // Store the map instance in the ref
+            mapRef.current = myMap;
 
-                myMap.on('click', function (event) { // Add click event listener to the map
-                    const { lat, lng } = event.latlng;
-                    setPosition({ latitude: lat, longitude: lng });// Update position in the parent component
-                    leaflet.marker([lat, lng]).addTo(myMap); // Add marker on map with a click
-                });
-            }}
-        }, [myLocation, setPosition]);
+            myMap.on('click', (event) => {
+                const { lat, lng } = event.latlng;
+                setPosition({ latitude: lat, longitude: lng }); // Update position in the parent component
+                leaflet.marker([lat, lng]).addTo(myMap); // Add marker on map with a click
+            });
+        }
+    }, [myLocation, setPosition]);
 
     return (
         <section id='map'></section>
